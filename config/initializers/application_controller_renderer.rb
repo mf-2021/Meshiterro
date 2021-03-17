e972a6d2bd3ed6e44262accb66d17b1d9a767f81
mf-2021 <manamu.fujiwara@gmail.com>
2021-03-17 02:12:00 +0000

    [Finish}4-5

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6489f12f28429f202b3e98b584bf18efde0a9240c86234e7ce837dc9de84b9130d8e384ff917f054381429966b991ab59320c9ed234232ae994bfec79bc6f39b'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
+  devise_for :users
+  root to: "post_images#index"
+  resources :post_images, only: [:new, :create, :index, :show] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
 end
